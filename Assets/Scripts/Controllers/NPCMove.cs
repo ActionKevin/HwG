28603b10bb7d066ea19705b47d358e2fc4291f5a
ActionKevin <33066860+ActionKevin@users.noreply.github.com>
2018-02-22 19:33:39 -0500

    Add files via upload
    NavMesh and basic guard put in

---
+++ Assets/Scripts/Controllers/NPCMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMove: MonoBehaviour 
+{
+	[SerializeField]
+	Transform _destination;
+
+	UnityEngine.AI.NavMeshAgent _navMeshAgent;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+		if (_navMeshAgent == null) {
+			Debug.LogError ("nav mesh is not attached to" + gameObject.name);
+		} 
+		else 
+		{
+			SetDestination ();
+		}
+	}
+	
+	// Update is called once per frame
+	private void SetDestination() 
+	{
+		if (_destination != null) 
+		{
+			Vector3 targetVector = _destination.transform.position;
+			_navMeshAgent.SetDestination (targetVector);
+		}
+	}
+}
